be7c48982e38456f133c1a77a93ba23b543a29d8
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-01-31 13:47:23 +0900

    template_redmine_plugin リポジトリの設定で更新した。

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -3,45 +3,56 @@ set -euo pipefail
 
 # Install dependencies
 sudo apt-get update -y
-sudo apt-get install -y imagemagick shellcheck zstd
+sudo apt-get install -y imagemagick nodejs npm shellcheck zstd
 
 # Configuration PATH
 mkdir -p ~/.local/bin
+# shellcheck disable=SC2016
 echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
 
+# Common
+GITHUB_HEADER_ACCEPT="Accept: application/vnd.github+json"
+GITHUB_HEADER_VERSION="X-GitHub-Api-Version: 2022-11-28"
+
 # Install actionlint
-ACTIONLINT_VERSION=1.7.7
-curl -fsSL -o - "https://github.com/rhysd/actionlint/releases/download/v${ACTIONLINT_VERSION}/actionlint_${ACTIONLINT_VERSION}_linux_amd64.tar.gz" | \
+ACTIONLIN_URL="https://api.github.com/repos/rhysd/actionlint/releases?per_page=1"
+ACTIONLINT_VERSION=$(curl -fsSL -H "${GITHUB_HEADER_ACCEPT}" -H "${GITHUB_HEADER_VERSION}" "${ACTIONLIN_URL}" | jq -r '.[0].tag_name')
+curl -fsSL -o - "https://github.com/rhysd/actionlint/releases/download/${ACTIONLINT_VERSION}/actionlint_${ACTIONLINT_VERSION#v}_linux_amd64.tar.gz" | \
     tar -zxf - -O "actionlint" > ~/.local/bin/actionlint
 chmod +x ~/.local/bin/actionlint
 
 # Install bat
-BAT_VERSION=0.25.0
-curl -fsSL -o - "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat-v${BAT_VERSION}-i686-unknown-linux-gnu.tar.gz" | \
-    tar -zxf - -O "bat-v${BAT_VERSION}-i686-unknown-linux-gnu/bat" > ~/.local/bin/bat
+BAT_URL="https://api.github.com/repos/sharkdp/bat/releases?per_page=1"
+BAT_VERSION=$(curl -fsSL -H "${GITHUB_HEADER_ACCEPT}" -H "${GITHUB_HEADER_VERSION}" "${BAT_URL}" | jq -r '.[0].tag_name')
+curl -fsSL -o - "https://github.com/sharkdp/bat/releases/download/${BAT_VERSION}/bat-${BAT_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | \
+    tar -zxf - -O "bat-${BAT_VERSION}-x86_64-unknown-linux-gnu/bat" > ~/.local/bin/bat
 chmod +x ~/.local/bin/bat
 
 # Install delta
-DELTA_VERSION=0.18.2
+DELTA_URL="https://api.github.com/repos/dandavison/delta/releases?per_page=1"
+DELTA_VERSION=$(curl -fsSL -H "${GITHUB_HEADER_ACCEPT}" -H "${GITHUB_HEADER_VERSION}" "${DELTA_URL}" | jq -r '.[0].tag_name')
 curl -fsSL -o - "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | \
     tar -zxf - -O "delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu/delta" > ~/.local/bin/delta
 chmod +x ~/.local/bin/delta
 
 # Install edit
-EDIT_VERSION=1.2.0
-curl -fsSL -o - "https://github.com/microsoft/edit/releases/download/v${EDIT_VERSION}/edit-${EDIT_VERSION}-x86_64-linux-gnu.tar.zst" | \
+#EDIT_URL="https://api.github.com/repos/microsoft/edit/releases?per_page=1"
+EDIT_VERSION="v1.2.0"
+curl -fsSL -o - "https://github.com/microsoft/edit/releases/download/${EDIT_VERSION}/edit-${EDIT_VERSION#v}-x86_64-linux-gnu.tar.zst" | \
     tar --zstd -xf - -O "edit" > ~/.local/bin/edit
 chmod +x ~/.local/bin/edit
 
 # Install lefthook
-LEFTHOOK_VERSION=1.11.13
-curl -fsSL -o - "https://github.com/evilmartians/lefthook/releases/download/v${LEFTHOOK_VERSION}/lefthook_${LEFTHOOK_VERSION}_Linux_x86_64.gz" | \
+LEFTHOOK_URL="https://api.github.com/repos/evilmartians/lefthook/releases?per_page=1"
+LEFTHOOK_VERSION=$(curl -fsSL -H "${GITHUB_HEADER_ACCEPT}" -H "${GITHUB_HEADER_VERSION}" "${LEFTHOOK_URL}" | jq -r '.[0].tag_name')
+curl -fsSL -o - "https://github.com/evilmartians/lefthook/releases/download/${LEFTHOOK_VERSION}/lefthook_${LEFTHOOK_VERSION#v}_Linux_x86_64.gz" | \
     gzip -c -d > ~/.local/bin/lefthook
 chmod +x ~/.local/bin/lefthook
 
 # Install yq
-YQ_VERSION=4.45.4
-curl -fsSL -o - "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_amd64.tar.gz" | \
+YQ_URL="https://api.github.com/repos/mikefarah/yq/releases?per_page=1"
+YQ_VERSION=$(curl -fsSL -H "${GITHUB_HEADER_ACCEPT}" -H "${GITHUB_HEADER_VERSION}" "${YQ_URL}" | jq -r '.[0].tag_name')
+curl -fsSL -o - "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64.tar.gz" | \
     tar -zxf - -O "./yq_linux_amd64" > ~/.local/bin/yq
 chmod +x ~/.local/bin/yq
 
@@ -49,6 +60,7 @@ chmod +x ~/.local/bin/yq
 REDMINE_URL=https://github.com/redmine/redmine.git
 
 sudo mkdir "${REDMINE_HOME}"
+sudo chown vscode:vscode "${REDMINE_HOME}"
 sudo chmod 755 "${REDMINE_HOME}"
 
 pushd "${REDMINE_HOME}"
